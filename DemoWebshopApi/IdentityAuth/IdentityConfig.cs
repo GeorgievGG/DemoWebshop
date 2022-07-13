2b641ee2d96e4fb00c0b65130bda3350b0a632c3
Georgi Georgiev <ggfinz@gmail.com>
2022-07-13 17:59:20 +0300

    Add Authentication logic and configure IdentityServer4.

---
+++ DemoWebshopApi/IdentityAuth/IdentityConfig.cs
@@ -0,0 +1,46 @@
+﻿using IdentityServer4.Models;
+using System.Security.Claims;
+
+namespace DemoWebshopApi.IdentityAuth
+{
+    public class IdentityConfig
+    {
+        public static IEnumerable<Client> Clients =>
+        new List<Client>
+        {
+           new Client
+                 {
+            ClientId = "DemoWebshopApi",
+            AllowOfflineAccess = true,
+
+            // no interactive user, use the clientid/secret for authentication
+            AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+            // secret for authentication
+            ClientSecrets =
+            {
+                new Secret("out_of_rangers_bros".Sha256())
+            },
+
+            // scopes that client has access to
+            AllowedScopes = { "users", "offline_access", "DemoWebshopApi", "roles" }
+                }
+
+        };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResource("roles", new[] { "role" })
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+          new List<ApiScope>
+          {
+                new ApiScope("users", "My API", new string[]{ ClaimTypes.NameIdentifier, ClaimTypes.Name, ClaimTypes.Role }),
+                new ApiScope("offline_access", "RefereshToken"),
+                new ApiScope("DemoWebshopApi", "app")
+          };
+    }
+}
--- DemoWebshopApi/Program.cs
+++ DemoWebshopApi/Program.cs
@@ -2,14 +2,18 @@ using DemoWebshopApi.Data;
 using DemoWebshopApi.Data.Entities;
 using DemoWebshopApi.Data.Interfaces;
 using DemoWebshopApi.Data.Repositories;
+using DemoWebshopApi.DTOs;
+using DemoWebshopApi.IdentityAuth;
 using DemoWebshopApi.Services.Interfaces;
 using DemoWebshopApi.Services.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.Configure<AuthSettings>(builder.Configuration.GetSection("AuthSettings"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -19,7 +23,7 @@ var connectionString = builder.Configuration.GetConnectionString("AppDb");
 builder.Services.AddDbContext<WebshopContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-builder.Services.AddTransient<IIDentityUserManager, IdentityUserManager>();
+builder.Services.AddTransient<IIdentityUserManager, IdentityUserManager>();
 builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddTransient<IProductService, ProductService>();
 builder.Services.AddTransient<IOrderService, OrderService>();
@@ -33,9 +37,52 @@ builder.Services.AddIdentityCore<User>(options =>
     options.Password.RequireNonAlphanumeric = false;
     options.Password.RequireUppercase = false;
 })
+                .AddRoles<ApplicationRole>()
                 .AddEntityFrameworkStores<WebshopContext>();
 
+builder.Services.AddIdentityServer((options) =>
+{
+    options.EmitStaticAudienceClaim = true;
+})
+
+    //This is for dev only scenarios when you don’t have a certificate to use.
+    .AddInMemoryApiScopes(IdentityConfig.ApiScopes)
+    .AddInMemoryClients(IdentityConfig.Clients)
+    .AddDeveloperSigningCredential()
+    .AddResourceOwnerValidator<PasswordValidator>();
+
+// omitted for brevity
+// Authentication
+// Adds the asp.net auth services
+builder.Services
+    .AddAuthorization(options =>
+    {
+        options.AddPolicy("Admin", policy =>
+                    policy.RequireRole("Admin"));
+
+        options.AddPolicy("User", policy =>
+                    policy.RequireRole("User"));
+    })
+
+    .AddAuthentication(options =>
+    {
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+
+    // Adds the JWT bearer token services that will authenticate each request based on the token in the Authorize header
+    // and configures them to validate the token with the options
+
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://localhost:7000";
+        options.Audience = "https://localhost:7000/resources";
+    });
+
 var app = builder.Build();
+
+app.UseIdentityServer();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -46,6 +93,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
