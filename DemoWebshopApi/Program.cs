e9d91e51064b0a970573ac024fc02f5cc500d556
Georgi Georgiev <ggfinz@gmail.com>
2022-07-14 02:10:18 +0300

    Configure swagger for authorization.

--- DemoWebshopApi/Program.cs
+++ DemoWebshopApi/Program.cs
@@ -8,6 +8,7 @@ using DemoWebshopApi.Services.Interfaces;
 using DemoWebshopApi.Services.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +18,36 @@ builder.Services.Configure<AuthSettings>(builder.Configuration.GetSection("AuthS
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(config =>
+{
+    config.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header (NO NEED for the word Bearer). Example: \"Authorization: {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http, //SecuritySchemeType.ApiKey
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+    config.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
 
 var connectionString = builder.Configuration.GetConnectionString("AppDb");
 builder.Services.AddDbContext<WebshopContext>(options => options.UseSqlServer(connectionString));
