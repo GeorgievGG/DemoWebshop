5308d3780aba1c16de92bb6822951318371a8b23
Georgi Georgiev <ggfinz@gmail.com>
2022-07-18 18:46:10 +0300

    Make sure about link is only shown when appropriate.

--- DemoWebshopPresentation/src/components/About.js
+++ DemoWebshopPresentation/src/components/About.js
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
-const About = () => {
+const About = ({ onGoBackClick }) => {
   return (
     <div>
         <h4>Version 1.0.0</h4>
-        <Link to='/'>Go Back</Link>
+        <Link onClick={onGoBackClick} to='/'>Go Back</Link>
     </div>
   )
 }
