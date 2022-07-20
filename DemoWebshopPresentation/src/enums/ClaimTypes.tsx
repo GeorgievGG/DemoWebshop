a202307fcd15071b0be3bcee4aff6fd7eceef360
Georgi Georgiev <ggfinz@gmail.com>
2022-07-20 18:24:17 +0300

    Create claim type enum to read claims more easily.

---
+++ DemoWebshopPresentation/src/enums/ClaimTypes.tsx
@@ -0,0 +1,7 @@
+enum ClaimTypes {
+    Username = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name",
+    UserRole = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+    UserId = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"
+}
+
+export default ClaimTypes
