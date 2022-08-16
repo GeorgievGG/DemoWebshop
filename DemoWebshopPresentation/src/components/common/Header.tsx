1975d4e0b87821ff8637b296fdf6b26eb8564760
Georgi Georgiev <juvetikon@abv.bg>
2022-08-16 19:52:58 +0300

    Add logic for handling batch processing results.

--- DemoWebshopPresentation/src/components/common/Header.tsx
+++ DemoWebshopPresentation/src/components/common/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import { selectProductsState, selectSessionState } from '../../store'
 import { flushPaymentState } from '../../store/paymentSlice'
 import { setProducts } from '../../store/productsSlice'
@@ -13,6 +14,31 @@ const Header = () => {
     const dispatch = useDispatch()
     const sessionState = useSelector<RootState, IUserSessionData>(selectSessionState)
     const products = useSelector<RootState, CatalogProductInfo[]>(selectProductsState)
+
+    const processPaymentBatches = async () => {
+        const response = await fetch('https://localhost:7000/api/Payment/ProcessBatch', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': `Bearer ${sessionState.Token}`
+            }
+        })
+      
+        const body = await response.text()
+        if (response.ok) {
+            const data = JSON.parse(body)
+            data?.successful?.length !== 0 && toast.success(`Payment batches processed successfully:\n${data.successful.join('\n')}`)
+            data?.failed?.length !== 0 && toast.warning(`Couldn't process payment batches: ${data.failed.join('\n')}`)
+        }
+        else {
+            let errorMessage = 'Unknown error'
+            if (body && body !== '') {
+                const data = JSON.parse(body)
+                errorMessage = data.message
+            }
+            toast.error(`Couldn't process payment batches: ${errorMessage}!`)
+        }
+    }
 
     const logout = async () => {
         dispatch(flushSessionData())
@@ -38,6 +64,9 @@ const Header = () => {
                 </li>
                 <li>
                     { sessionState.LoggedUserRole === "Admin" && <Button className="btn btn-dark" text={"Users"} onClick={() => navigate("/userList")} /> }
+                </li>
+                <li>
+                    { sessionState.LoggedUserRole === "Admin" && <Button className="btn btn-dark" text={"Process Payment Batches"} onClick={processPaymentBatches} /> }
                 </li>
                 <li>
                     { sessionState.LoggedUserRole === "Admin" && <Button className="btn btn-dark" text={"Orders"} onClick={() => navigate("/orderList")} /> }
