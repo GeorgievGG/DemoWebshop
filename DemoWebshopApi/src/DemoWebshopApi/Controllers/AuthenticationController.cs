1364fd731204b4f00b423fad2d4e6f8de7c32d8d
Georgi Georgiev <Georgi.Georgiev@ingenico.com>
2023-06-17 16:20:37 +0300

    Refactor IS4 calls

--- DemoWebshopApi/src/DemoWebshopApi/Controllers/AuthenticationController.cs
+++ DemoWebshopApi/src/DemoWebshopApi/Controllers/AuthenticationController.cs
@@ -26,13 +26,15 @@ namespace DemoWebshopApi.Controllers
 
             var url = "https://localhost:7000/connect/token";
 
-            var identityServerParameters = new List<KeyValuePair<string, string>>();
-            identityServerParameters.Add(new KeyValuePair<string, string>("grant_type", _authSettings.Value.GrantTypeLogin));
-            identityServerParameters.Add(new KeyValuePair<string, string>("username", loginModel.Username));
-            identityServerParameters.Add(new KeyValuePair<string, string>("password", loginModel.Password));
-            identityServerParameters.Add(new KeyValuePair<string, string>("client_id", _authSettings.Value.ClientId));
-            identityServerParameters.Add(new KeyValuePair<string, string>("client_secret", _authSettings.Value.ClientSecret));
-            identityServerParameters.Add(new KeyValuePair<string, string>("scope", _authSettings.Value.Scope));
+            var identityServerParameters = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("grant_type", _authSettings.Value.GrantTypeLogin),
+                new KeyValuePair<string, string>("username", loginModel.Username),
+                new KeyValuePair<string, string>("password", loginModel.Password),
+                new KeyValuePair<string, string>("client_id", _authSettings.Value.ClientId),
+                new KeyValuePair<string, string>("client_secret", _authSettings.Value.ClientSecret),
+                new KeyValuePair<string, string>("scope", _authSettings.Value.Scope)
+            };
 
             using (client)
             {
@@ -51,11 +53,13 @@ namespace DemoWebshopApi.Controllers
 
             var url = "https://localhost:7000/connect/token";
 
-            var identityServerParameters = new List<KeyValuePair<string, string>>();
-            identityServerParameters.Add(new KeyValuePair<string, string>("grant_type", _authSettings.Value.GrantTypeRefresh));
-            identityServerParameters.Add(new KeyValuePair<string, string>("client_id", _authSettings.Value.ClientId));
-            identityServerParameters.Add(new KeyValuePair<string, string>("client_secret", _authSettings.Value.ClientSecret));
-            identityServerParameters.Add(new KeyValuePair<string, string>("refresh_token", refreshToken));
+            var identityServerParameters = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("grant_type", _authSettings.Value.GrantTypeRefresh),
+                new KeyValuePair<string, string>("client_id", _authSettings.Value.ClientId),
+                new KeyValuePair<string, string>("client_secret", _authSettings.Value.ClientSecret),
+                new KeyValuePair<string, string>("refresh_token", refreshToken)
+            };
 
             using (client)
             {
